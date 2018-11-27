1d366f74989474f84f0ffc947d46730a736593fe
liutaotao <2585881917@qq.com>
2018-11-27 20:13:14 +0800

    11

---
+++ js/jQuery_SuperPlus-1.0.js
@@ -0,0 +1,17 @@
+;(function(method){
+	method(window,window.document,jQuery);
+}(function(win,doc,$){
+	$.fn.Superplus=function(options){
+		//默认参数
+		var setting={
+			length:3,
+			color:"blue"
+		};
+		$.extend(setting,options);
+		return $.each(this, function(index,obj) {
+			$("<span/>").html("+").css("cursor","pointer").css("color",setting.color).click(function(){
+				$(obj).width($(obj).width()+setting.length);
+			}).insertAfter(obj);
+		});
+	}
+}))
---
+++ js/jQuery_SuperPlus1-1.0.js
@@ -0,0 +1,39 @@
+;(function(method){
+	method(window,window.document,jQuery);
+}(function(win,doc,$){
+	$.fn.Supman=function(options){
+		//默认参数
+		var setting={
+			length:100,
+			background:"lightblue",
+			changeAfter:null,
+			width:3,
+			color:"#000",
+			before:null,
+			adding:null,
+			after:null,
+			css:"Supman"
+		}
+		//使用用户名参数覆盖参数
+		$.extend(setting,options);
+		return $.each(this, function(index,obj) {
+			$("<span/>").html("+").css("background",setting.background).addClass(setting.css).css("cursor","pointer").click(function(){
+				var obj=$(this).prev();
+				if(setting.before){
+					setting.before(obj,setting,this);
+				}
+				var isFire=true;
+				if(setting.adding){
+					isFire=setting.adding(obj,setting,this);
+				}
+				if(isFire){
+					obj.width(obj.width()+setting.width);
+				}
+//				$(obj).width($(obj).width()+setting.length);
+//				if(setting.changeAfter){
+//					setting.changeAfter($(obj));
+//				}
+			}).insertAfter(obj);
+		});
+	}
+}))
